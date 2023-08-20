3b5ef85a2d736a1c4f096bcc070bbd2e1d6ad688
dayshaunlee <leedayshaun@gmail.com>
2023-08-20 12:26:36 -0700

    working implementation

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from PIL import Image
 
@@ -31,7 +33,48 @@ def rearrange_tiles(image_path: str, tile_size: tuple[int, int], ordering: list[
     once. If these conditions do not hold, raise a ValueError with the message:
     "The tile size or ordering are not valid for the given image".
     """
-    image = Image.open(image_path)
-    width, height = image.size
+    im = Image.open(image_path)
+    width, height = im.size
     if not valid_input((width, height), tile_size, ordering):
         raise ValueError("The tile size or ordering are not valid for the given image")
+
+    tile_width, tile_height = tile_size
+    number_of_columns = width // tile_width
+    # number_of_rows = height // tile_height
+
+    result = Image.new(im.mode, (width, height))
+    for i in ordering:
+        n = ordering.index(i)
+
+        new_column, new_row = get_position(n, number_of_columns)
+        source_column, source_row = get_position(i, number_of_columns)
+
+        new_locations = get_coordinates(new_row, new_column, tile_height, tile_width)
+        source_locations = get_coordinates(source_row, source_column, tile_height, tile_width)
+
+        tile = im.crop(source_locations)
+        result.paste(tile, new_locations)
+
+    result.show()
+    result.save(out_path, 'PNG')
+
+
+def get_coordinates(row, column, tile_height, tile_width) -> tuple[int, int, int, int]:
+    """
+    returns the coordinates (left, upper, right, lower) of a tile, given its position and a tiles size
+    """
+    left = column * tile_width
+    right = left + tile_width
+    upper = row * tile_height
+    lower = upper + tile_height
+    return left, upper, right, lower
+
+
+def get_position(i, number_of_columns) -> tuple[int, int]:
+    """
+    returns the row/column numbers (0-indexed), given a tiles index
+    and the number of columns
+    """
+    rows = math.floor(i / number_of_columns)
+    cols = i - rows * number_of_columns
+    return cols, rows
